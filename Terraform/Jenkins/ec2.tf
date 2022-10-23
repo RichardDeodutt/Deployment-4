ab8fd03b610b7f36da740268c4b460ca2239a934
Richard Deodutt <richarddeodutt@gmail.com>
2022-10-23 05:19:06 -0400

    Userdata for ec2

--- Terraform/Jenkins/ec2.tf
+++ Terraform/Jenkins/ec2.tf
@@ -1,14 +1,17 @@
 resource "aws_instance" "jenkins" {
-  ami = var.ami
-  instance_type = var.itype
-  associate_public_ip_address = var.publicip
-  key_name = var.keyname
-
-  security_groups = [
-    var.secgroupname
-  ]
-
-  tags = {
-    Name = var.ec2name
-  }
+    ami = var.ami
+    instance_type = var.itype
+    associate_public_ip_address = var.publicip
+    key_name = var.keyname
+    user_data = <<EOF
+                #!/bin/bash
+                curl -s -O https://raw.githubusercontent.com/RichardDeodutt/Deployment-4/main/Runners/runinstalljenkins.sh && sudo chmod +x runinstalljenkins.sh && sudo ./runinstalljenkins.sh
+                EOF
+    security_groups = [
+        var.secgroupname
+    ]
+    
+    tags = {
+        Name = var.ec2name
+    }
 }
