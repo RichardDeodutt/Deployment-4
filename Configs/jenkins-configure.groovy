9f309da0de4049dad80658d4e2661f0c83ad1255
Richard Deodutt <richarddeodutt@gmail.com>
2022-10-24 23:58:29 -0400

    Fixed security problems

--- Configs/jenkins-configure.groovy
+++ Configs/jenkins-configure.groovy
@@ -9,6 +9,12 @@ import jenkins.install.*
 //Locate the jenkins instance running
 def JInstance = Jenkins.getInstance()
 
+//Get the default admin user
+User Jadmin = User.get('admin')
+
+//Delete the default admin user
+Jadmin.delete()
+
 //Required to have a realm to create a new user
 def JHudsonRealm = new HudsonPrivateSecurityRealm(false)
 
@@ -21,20 +27,11 @@ JInstance.setSecurityRealm(JHudsonRealm)
 //Needs a strategy to include admin access
 def JStrategy = new FullControlOnceLoggedInAuthorizationStrategy()
 
-//Add the user to the strategy and give admin permissions
-JStrategy.add(Jenkins.ADMINISTER, ~JenkinsUsername~)
-
 //Extra Security
 JStrategy.setAllowAnonymousRead(false)
 
 //Apply the strategy containing the created user to the running jenkins instance
 JInstance.setAuthorizationStrategy(JStrategy)
-
-//Get the default admin user
-User Jadmin = User.get('admin')
-
-//Delete the default admin user
-Jadmin.delete()
 
 //Set the state of installation to completed skipping setup
 JInstance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
--- Scripts/configurejenkins.sh
+++ Scripts/configurejenkins.sh
@@ -46,15 +46,13 @@ main(){
     cat "jenkins-configure.groovy" | sed "s/~JenkinsUsername~/$(cat JENKINS_USERNAME)/g" | sed "s/~JenkinsPassword~/$(cat JENKINS_PASSWORD)/g" > "jenkins-configure.groovy" && logokay "Successfully set configure groovy script for ${Name}" || { logerror "Failure setting configure groovy script for ${Name}" && exiterror ; }
 
     #Remote execute the groovy script
-    #test $(
-    # | wc -c) -eq 0
-    curl -s -b JenkinsSessionCookie -X POST http://localhost:8080/scriptText  -H "Jenkins-Crumb: $(cat JenkinsLastCrumb)" --user admin:$(cat /var/lib/jenkins/secrets/initialAdminPassword) --data-urlencode "script=$( < ./jenkins-configure.groovy)" && logokay "Successfully executed configure groovy script for ${Name}" || { logerror "Failure executing configure groovy script for ${Name}" && exiterror ; }
+    curl -s -b JenkinsSessionCookie -X POST http://localhost:8080/scriptText  -H "Jenkins-Crumb: $(cat JenkinsLastCrumb)" --user admin:$(cat /var/lib/jenkins/secrets/initialAdminPassword) --data-urlencode "script=$( < ./jenkins-configure.groovy)" > JenkinsExecution && test $(cat JenkinsExecution | wc -c) -eq 0 && logokay "Successfully executed configure groovy script for ${Name}" || { logerror "Failure executing configure groovy script for ${Name}" && cat JenkinsExecution && rm JenkinsExecution && exiterror ; }
 
     #Remove configure groovy script
     rm jenkins-configure.groovy && logokay "Successfully removed configure groovy script for ${Name}" || { logerror "Failure removing configure groovy script for ${Name}" && exiterror ; }
 
-    #Remove initialAdminPassword
-    rm /var/lib/jenkins/secrets/initialAdminPassword && logokay "Successfully removed initialAdminPassword for ${Name}" || { logerror "Failure removing initialAdminPassword for ${Name}" && exiterror ; }
+    #Remove initialAdminPassword and JenkinsExecution
+    rm /var/lib/jenkins/secrets/initialAdminPassword ; rm JenkinsExecution && logokay "Successfully removed initialAdminPassword for ${Name}" || { logerror "Failure removing initialAdminPassword for ${Name}" && exiterror ; }
 }
 
 #Log start
