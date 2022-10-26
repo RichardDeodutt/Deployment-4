60de7eb38d555a806b134eaf4c0ffe1f0679178c
Richard Deodutt <richarddeodutt@gmail.com>
2022-10-26 00:40:22 -0400

    jenkins jar cli

--- Scripts/saferestartjenkins.sh
+++ Scripts/saferestartjenkins.sh
@@ -36,6 +36,9 @@ Retry=15
 #Timeout in seconds, 10 minutes
 Timeout=600
 
+#Jenkins cli jar file name
+JCJ = "jenkins-cli.jar"
+
 #The main function
 main(){
     #Update local apt repo database
@@ -46,6 +49,9 @@ main(){
 
     #Install curl if not already
     aptinstalllog "curl"
+
+    #Download jenkins-cli.ja if not already
+    ls $JCJ > /dev/null 2>&1 && logokay "Successfully found $JCJ for ${Name}" || { curl -s "http://localhost:8080/jnlpJars/jenkins-cli.jar" -O -J && ls $JCJ > /dev/null 2>&1 && logokay "Successfully downloaded $JCJ for ${Name}" || { logerror "Failure obtaining $JCJ for ${Name}" && exiterror ; } ; }
 
     #Start the service if not already
     systemctl start jenkins > /dev/null 2>&1 && logokay "Successfully started ${Name}" || { logerror "Failure starting ${Name}" && exiterror ; }
@@ -64,7 +70,7 @@ main(){
     sleep $Retry
 
     #Remote check the updateCenter jobs, refresh jobs and store to file
-    curl -s -g -b JenkinsSessionCookie -X GET "http://localhost:8080/updateCenter/api/json?tree=jobs[*]" -H "Jenkins-Crumb: $(cat JenkinsLastCrumb)" --user $JENKINS_USERNAME:$JENKINS_PASSWORD | jq -r '. | .jobs | .[].status._class? // empty' | sed 's/hudson.model.UpdateCenter$DownloadJob$SuccessButRequiresRestart//g' | sed 's/hudson.model.UpdateCenter$DownloadJob$Success//g' | sed '/^$/d' > JenkinsExecution && logokay "Successfully checked jobs for ${Name}, $(printwarning $(cat JenkinsExecution | wc -l)) jobs left" || { logerror "Failure checking jobs for ${Name}" && cat JenkinsExecution && rm JenkinsExecution && exiterror ; }
+    curl -s -g -b JenkinsSessionCookie -X GET "http://localhost:8080/updateCenter/api/json?tree=jobs[*]" -H "Jenkins-Crumb: $(cat JenkinsLastCrumb)" --user $JENKINS_USERNAME:$JENKINS_PASSWORD | jq -r '. | .jobs | .[].status._class? // empty' | sed 's/hudson.model.UpdateCenter$DownloadJob$SuccessButRequiresRestart//g' | sed 's/hudson.model.UpdateCenter$DownloadJob$Success//g' | sed '/^$/d' > JenkinsExecution && logokay "Successfully checked jobs for ${Name}, $(printwarning $(cat JenkinsExecution | wc -l)) $(printokay 'jobs left')" || { logerror "Failure checking jobs for ${Name}" && cat JenkinsExecution && rm JenkinsExecution && exiterror ; }
 
     #Store the pending jobs as a variable and refresh it
     PendingJobs=$(cat JenkinsExecution)
@@ -80,7 +86,7 @@ main(){
     done
 
     #Remote do a safe restart
-    curl -s -b JenkinsSessionCookie -X GET "http://localhost:8080/safeRestart" -H "Jenkins-Crumb: $(cat JenkinsLastCrumb)" --user $JENKINS_USERNAME:$JENKINS_PASSWORD > JenkinsExecution && test $(cat JenkinsExecution | wc -c) -eq 0 && logokay "Successfully executed safe restart for ${Name}" || { logerror "Failure executing safe restart for ${Name}" && cat JenkinsExecution && rm JenkinsExecution && exiterror ; }
+    java -jar $JCJ -s "http://localhost:8080" -http -auth $JENKINS_USERNAME:$JENKINS_PASSWORD && logokay "Successfully executed safe restart for ${Name}" || { logerror "Failure executing safe restart for ${Name}" && exiterror ; }
 }
 
 #Log start
